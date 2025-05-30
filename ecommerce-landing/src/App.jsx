b4cff17d2f5cfe3346a85a2beec376e39ab3a908
LaganMehtaJi <laganmehta.in@gmail.com>
2025-05-30 17:59:03 +0530

    Create React App For Landing Page

---
+++ ecommerce-landing/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import NavBar from "./Components/NavBar.jsx";
+import Hero from './Screen/Hero.jsx';
+import Products from './Screen/Products.jsx';
+function App() {
+
+  return (
+    <>
+     <NavBar/>
+     <Hero/>
+     <Products/>
+    </>
+  )
+}
+
+export default App
